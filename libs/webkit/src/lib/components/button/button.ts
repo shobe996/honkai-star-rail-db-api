ffd26836a324a17c479ce2c5bbf9c76f6ff20469
ssogorovic <ssogorovic@gmail.com>
2026-05-10 12:34:10 +0200

    feat: implement support for round button

--- libs/webkit/src/lib/components/button/button.ts
+++ libs/webkit/src/lib/components/button/button.ts
@@ -1,16 +1,18 @@
+import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'lib-button',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.html',
   styleUrl: './button.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {
-  variant = input<'primary' | 'secondary' | 'reset' | 'nav'>('primary');
+  variant = input<'primary' | 'secondary' | 'reset' | 'nav' | 'round'>('primary');
   disabled = input<boolean>(false);
   type = input<'button' | 'submit' | 'reset'>('button');
+  active = input<boolean>(false);
 
   isEdge = input<boolean>(false);
 }
